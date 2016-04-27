57639c5bed483c7aacc5c2fd5e4e439a9a548ecb
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-28 00:57:01 +0200

    gestion message emision et réceptio

--- public/client.js
+++ public/client.js
@@ -1,1 +1,18 @@
 var socket = io();
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    var message = {
+        text : $('#m').val()
+    }
+    socket.emit('chat-message', message);
+    $('#m').val('');
+    if (message.text.trim().length !== 0) {
+        socket.emit('chat-message', message);
+    }
+    $('#chat input').focus();
+});
+
+socket.on('chat-message', function (message) {
+    $('#messages').append($('<li>').text(message.text));
+});
--- server.js
+++ server.js
@@ -6,12 +6,26 @@ var io = require('socket.io')(http);
 
 app.use("/", express.static(__dirname + "/public"));
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
+
+
+    socket.on('chat-message', function (message) {
+        console.log('message : ' + message.text);
+    });
+
+    socket.on('chat-message', function (message) {
+        io.emit('chat-message', message);
+    });
+
+
 });
-http.listen(3000, function(){
+
+
+http.listen(3000, function () {
     console.log('Server is listening on *:3000');
 });
+
