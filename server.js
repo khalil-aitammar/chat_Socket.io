5f7c6905e33faf645676ffa8c6875eb50782dda0
khalil ait ammar <ait.ammar.khalil@gmail.com>
2016-04-28 00:06:55 +0200

    ajour de socket et creaction de socket connection et déconnection

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 app.use("/", express.static(__dirname + "/public"));
 
-
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
 http.listen(3000, function(){
     console.log('Server is listening on *:3000');
 });
